e001699f091ea73bf0766cba719e14dbe7468127
leeminho <tommymh97@naver.com>
2025-04-04 01:37:17 +0900

    calll

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,10 +1,37 @@
 package calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(str, ",|:");
+        for (int i = 0; i <st.countTokens(); i++){
+            sum += Integer.parseInt(st.nextToken());
+        }
+
+
+
+
+
 
 
 
     }
+}
+
+class baseGubun{
+    String a = ",";
+    String b = ":";
+}
+
+class customGubun{
+
 }
